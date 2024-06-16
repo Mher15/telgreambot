22f474141b343f296424f1d855e42401735cfb58
Mher15 <mher.janikyan01@gmail.com>
2024-06-16 17:57:43 +0400

    /

--- api/webhook.js
+++ api/webhook.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
             const chatId = body.message.chat.id;
             const responseText = "Welcome to my Telegram bot! Click the link to visit the app: https://your-vercel-app.vercel.app";
 
-            const sendMessageUrl = `https://api.telegram.org/bot963717429/sendMessage`;
+            const sendMessageUrl = `https://api.telegram.org/bot7226911794:AAFzsxnXyd-3hfLZs4xmSZ1j7BgLvm50I54/sendMessage`;
 
             await fetch(sendMessageUrl, {
                 method: 'POST',
